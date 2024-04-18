bf14ab14e50d53191b66fd1a1e555b8fb58df163
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2024-04-18 04:39:00 +0200

    add Api

---
+++ src/helpers/api.js
@@ -0,0 +1,10 @@
+export const BASE_URL = "https://petlove.b.goit.study/api";
+export const REGISTER_URL = "/users/signup";
+export const LOGIN_URL = "/users/signin";
+export const CURRENT_USER_URL = "/users/current";
+export const CURRENT_USER_FULL_URL = "/users/current/full";
+export const LOGOT_URL = "/users/signout";
+export const NEWS_URL = "news";
+export const NOTICES_URL = "notices";
+export const ADD_FAVORITES_NOTICES_URL = "notices/favorites/add";
+export const FRIENDS_URL = "friends";
---
+++ src/helpers/pets.js
@@ -0,0 +1,18 @@
+import registrationAvatar from "../helpers/images/registrationAvatar.png";
+import loginAvatar from "../helpers/images/loginAvatar.png";
+
+export const registrationPet = {
+  avatar: registrationAvatar,
+  name: "Jack",
+  birthday: "18.10.2021",
+  description:
+    "Jack is a gray Persian cat with green eyes. He loves to be pampered and groomed, and enjoys playing with toys.",
+};
+
+export const loginPet = {
+  avatar: loginAvatar,
+  name: "Rich",
+  birthday: "21.09.2020",
+  description:
+    "Rich would be the perfect addition to an active family that loves to play and go on walks. I bet he would love having a doggy playmate too!",
+};
