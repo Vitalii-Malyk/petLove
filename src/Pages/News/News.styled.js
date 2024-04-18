7652245995ca618c8d07b3621fb314106986f6c5
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2024-04-18 04:39:51 +0200

    add homePage, newsPage

---
+++ src/Pages/News/News.styled.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+  margin: 0 auto;
+  padding-top: 64px;
+  padding-bottom: 80px;
+  padding-left: 30px;
+  padding-right: 30px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 60px;
+`;
---
+++ src/Pages/News/index.jsx
@@ -0,0 +1,44 @@
+import { Container } from "components/Container/Container";
+import { NewsList } from "components/NewsList/NewsList";
+import { Pagination } from "components/Pagination/Pagination";
+import { SearchField } from "components/SearchField/SearchField";
+import { Title } from "components/Title/Title";
+import { Section, Wrapper } from "./News.styled";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
+import { fetchNews } from "../../redux/news/newsOperations";
+import { selectNews, selectTotalPages } from "../../redux/news/newsSelectors";
+import { topScroll } from "utils/topScroll";
+
+const News = () => {
+  const [selectedPage, setSelectedPage] = useState(1);
+  const dispatch = useDispatch();
+  const news = useSelector(selectNews);
+  const totalPages = useSelector(selectTotalPages);
+
+  const handlePageChange = (e) => {
+    // console.log(`Selected page: ${page}`);
+    // setSelectedPage(page);
+    setSelectedPage(e.selected + 1);
+    topScroll();
+  };
+
+  useEffect(() => {
+    dispatch(fetchNews(selectedPage));
+  }, [dispatch, selectedPage]);
+
+  return (
+    <Section>
+      <Container>
+        <Wrapper>
+          <Title name={"News"} />
+          <SearchField />
+        </Wrapper>
+        <NewsList news={news} />
+        <Pagination totalPages={totalPages} onPageChange={handlePageChange} />
+      </Container>
+    </Section>
+  );
+};
+
+export default News;
