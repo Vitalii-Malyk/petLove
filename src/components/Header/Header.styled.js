1faa780f945770805aa7d1d5f0af9825c6498217
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2024-04-18 04:43:14 +0200

    add header

---
+++ src/components/Header/Header.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const HeaderWrapper = styled.header``;
+
+export const Navigation = styled.nav`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 32px;
+  padding-bottom: 32px;
+`;
+
+export const Link = styled(NavLink)``;
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import { Logo } from "components/Logo/Logo";
+import { HeaderWrapper, Link, Navigation } from "./Header.styled";
+import { Container } from "components/Container/Container";
+import { Nav } from "./Nav/Nav";
+import { AuthNav } from "./AuthNav/AuthNav";
+import { useSelector } from "react-redux";
+import { selectIsLoggedIn } from "../../redux/auth/authSelectors";
+import { UserNav } from "./UserNav/UserNav";
+
+export const Header = () => {
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  return (
+    <HeaderWrapper>
+      <Container>
+        <Navigation>
+          <Link to="/home">
+            <Logo />
+          </Link>
+          <Nav />
+          {isLoggedIn ? <UserNav /> : <AuthNav />}
+        </Navigation>
+      </Container>
+    </HeaderWrapper>
+  );
+};
