5a570966ba2d8a15174ffd7c4a5a70eb0b9a7a53
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2024-04-18 04:42:08 +0200

    add hero

---
+++ src/components/Hero/Hero.styled.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { theme } from "constants/theme";
+
+export const HeroWrapper = styled.div``;
+
+export const TextWrapper = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 73px;
+  background-color: ${theme.colors.accent};
+  padding: 80px 64px 32px 64px;
+  border-radius: 60px;
+`;
+
+export const Title = styled.h1`
+  color: ${theme.colors.white};
+  font-size: ${theme.fontSizes.xxxxxl};
+  font-weight: ${theme.fontWeight.bold};
+  line-height: 0.96667;
+  letter-spacing: -2.7px;
+  width: 760px;
+`;
+
+export const Text = styled.p`
+  color: ${theme.colors.white};
+  font-size: ${theme.fontSizes.s};
+  font-weight: ${theme.fontWeight.medium};
+  line-height: 1.22222;
+  letter-spacing: -0.36px;
+  width: 255px;
+`;
+
+export const Accent = styled.span`
+  color: ${theme.colors.accentHero};
+`;
+
+export const Image = styled.img`
+  border-radius: 60px;
+`;
---
+++ src/components/Hero/index.jsx
@@ -0,0 +1,27 @@
+import heroImg from "../../images/heroImg.jpg";
+import {
+  Accent,
+  HeroWrapper,
+  Image,
+  Text,
+  TextWrapper,
+  Title,
+} from "./Hero.styled";
+
+export const Hero = () => {
+  return (
+    <HeroWrapper>
+      <TextWrapper>
+        <Title>
+          Take good <Accent>care</Accent> of your small pets
+        </Title>
+        <Text>
+          Choosing a pet for your home is a choice that is meant to enrich your
+          life with immeasurable joy and tenderness.
+        </Text>
+      </TextWrapper>
+
+      <Image src={heroImg} alt="Girl with a dog" />
+    </HeroWrapper>
+  );
+};
