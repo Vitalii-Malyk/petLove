87bc3004e07da09c8c0f1531659d4fd510aba5ce
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2024-04-18 04:41:38 +0200

    add layout

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,5 +1,19 @@
+import { Suspense } from "react";
+import { Outlet } from "react-router-dom";
+import { Loader } from "../Loader";
+import { Header } from "../Header";
+
 const Layout = () => {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </main>
+    </>
+  );
 };
 
 export default Layout;
