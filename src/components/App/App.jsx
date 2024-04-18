d0cfc2d8c6eb6bb8554c5fbc6f237129750f059e
Vitalii-Malyk <125901006+Vitalii-Malyk@users.noreply.github.com>
2024-04-18 04:43:31 +0200

    fix app

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,27 +1,35 @@
 import { Routes, Route } from "react-router-dom";
+import { lazy, useEffect } from "react";
+import Layout from "../Layout/";
+import { useDispatch, useSelector } from "react-redux";
+import { selectIsRefreshing } from "../redux/auth/authSelectors";
+import { refreshUser } from "../redux/auth/authOperation";
+import { Loader } from "./Loader/Loader";
 
-import "./App.css";
+const Home = lazy(() => import("../pages/Home/Home"));
+const News = lazy(() => import("../pages/News/News"));
 
-function App() {
-  return (
+const App = () => {
+  const dispatch = useDispatch();
+  const isRefreshing = useSelector(selectIsRefreshing);
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <Loader />
+  ) : (
     <>
       <Routes>
-        {/* <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="/home" element={<Home />} />
           <Route path="/news" element={<News />} />
-          <Route path="/notices" element={<Notices />} />
-          <Route path="/friends" element={<Friends />} />
-          <Route path="/register" element={<Registration />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/add-pet" element={<AddPet />} />
-          <Route path="*" element={<NotFound />} /> */}
-        {/* </Route> */}
+        </Route>
       </Routes>
       {/* <ToastContainer /> */}
     </>
   );
-}
-
+};
 export default App;
